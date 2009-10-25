f354e54a0c512181e463d5fa6040f14041e41ef8
David.Stolarsky <David.Stolarsky@1631959e-bd7d-11de-a8ef-132414f8baa6>
2009-10-25 17:46:10 +0000

    function to do entire category

---
+++ code/tasks/crawler/crawlCategories.py
@@ -0,0 +1,19 @@
+import webcrawlerPiratebayLS
+
+# scrape procedure!
+
+tpb = webcrawlerPiratebayLS.webcrawlerTorrent()
+
+
+tpb.recordActivityForCategory(205)
+
+#for cat in tpb.subCategories:
+#	print "-------------\n%s\n-------------" % cat
+#	print tpb.getBrowseLeechersCatPage(cat)
+
+#tpb = webcrawlerTorrent()
+#data = tpb.getSeedersAndLeechers('5130018')
+#print data['seeders']
+#print data['leechers']
+#tpb.getTopCatPage(100)
+#print time.localtime()
